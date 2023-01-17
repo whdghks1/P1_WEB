f7df56320e270ca3a8f676272301799b1ea82f3e
whdghks1 <johjhj0530@gmail.com>
2023-01-17 20:28:12 +0900

    main 수정

--- front/src/screens/Main/index.js
+++ front/src/screens/Main/index.js
@@ -12,7 +12,6 @@ export default function Main() {
                 <p>Syulist/Styling</p>
                 <p>Shop</p>
                 <p>My Page</p>
-                <p>r귀찮아</p>
             </header>
         </div>
     );
