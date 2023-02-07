1c98a5de3c43a12d8e40c11d6e1293ef0b3f52a5
whdghks1 <johjhj0530@gmail.com>
2023-02-07 22:40:21 +0900

    gk.....

--- front/pages/_document.tsx
+++ front/pages/_document.tsx
@@ -2,12 +2,6 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
+    < />
   )
 }
