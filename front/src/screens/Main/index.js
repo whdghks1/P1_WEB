73fdbbba98fc7a33c335a2bf8bd2e75a966b6a5c
whdghks1 <johjhj0530@gmail.com>
2023-02-11 20:44:45 +0900

    banner

--- front/src/screens/Main/index.js
+++ front/src/screens/Main/index.js
@@ -19,6 +19,7 @@ export default function Main() {
                 <p>Shop</p>
                 <p>My Page</p>
             </StyledHeader>
+            <banner></banner>
             <bottom>instargram</bottom>
         </>
     );
