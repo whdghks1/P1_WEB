ab1cd263ff653bf1c5043cc03653b83cf8101c72
whdghks1 <johjhj0530@gmail.com>
2023-02-15 23:13:12 +0900

    하 하기싫어

--- front/src/screens/Main/index.js
+++ front/src/screens/Main/index.js
@@ -20,6 +20,9 @@ export default function Main() {
                 <p>My Page</p>
             </StyledHeader>
             <banner></banner>
+            <banner></banner>
+            <banner></banner>
+            <banner></banner>
             <bottom>instargram</bottom>
         </>
     );
