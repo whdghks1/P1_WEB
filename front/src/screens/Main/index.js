d35651a5988ee8664e10c3d850e4ab43e29631d4
whdghks1 <johjhj0530@gmail.com>
2023-03-12 23:07:33 +0900

    힘들구만

--- front/src/screens/Main/index.js
+++ front/src/screens/Main/index.js
@@ -13,11 +13,21 @@ export default function Main() {
     return (
         <>
             <StyledHeader>
-                <image href='./src/image/logo'></image>
-                <p>Brand Story</p>
-                <p>Syulist/Styling</p>
-                <p>Shop</p>
-                <p>My Page</p>
+                <div>
+                    <div>
+                        <p>이용안내</p>
+                        <p>고객센터</p>
+                        <p>로그인</p>
+                        <p>회원가입</p>
+                    </div>
+                    <div>
+                        <image href='./src/image/logo'></image>
+                        <p>Brand Story</p>
+                        <p>Syulist/Styling</p>
+                        <p>Shop</p>
+                        <p>My Page</p>
+                    </div>
+                </div>
             </StyledHeader>
             <section>
                 <banner></banner>
